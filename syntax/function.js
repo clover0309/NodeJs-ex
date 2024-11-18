3fc966e63d6b29b89f57cedc3d70ba7bdf2e5843
Byul. <uio400@naver.com>
2024-11-18 17:04:10 +0900

    Add files via upload

---
+++ syntax/function.js
@@ -0,0 +1,15 @@
+f123();
+console.log('A');
+console.log('Z');
+console.log('B');
+f123();
+console.log('C');
+console.log('D');
+console.log('E');
+f123();
+
+function f123() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
---
+++ syntax/function2.js
@@ -0,0 +1,8 @@
+//반올림
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
+
+function sum(first, second) { //()안에 들어가는 것을 파라미터.
+    return first+second; // 리턴을 만나면 실행이 저절로 종료된다.
+}
+console.log(sum(2,4)); //()안에 들어가는 것을 인자.
